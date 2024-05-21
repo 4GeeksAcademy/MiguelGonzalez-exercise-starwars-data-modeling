9933aab0b7c67bd5f3b34d8da9d7535077f31d57
Miguel González <152456005+Miguel-Gonzalez01@users.noreply.github.com>
2024-05-21 23:24:26 +0000

    Terminado

--- src/models.py
+++ src/models.py
@@ -7,26 +7,44 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+class User(Base):
     __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+    favorites = relationship("Favorites")
+    email = Column(String(100), nullable=False)
+    password = Column(String(100), nullable=False)
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Favorites(Base):
+    __tablename__ = 'favorites'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
+    planetas = relationship("Planetas")
+    personajes = relationship("Personajes")
     def to_dict(self):
         return {}
 
+class Personajes(Base):
+    __tablename__ = 'personajes'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    height = Column(String(100)) 
+    mass = Column(String(100)) 
+    hair_color = Column(String(100)) 
+    eye_color = Column(String(100)) 
+    birth_year = Column(String(100)) 
+    gender = Column(String(100)) 
+
+class Planetas(Base):
+    __tablename__ = 'planetas'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    rotation_period = Column(String(100)) 
+    orbital_period = Column(String(100)) 
+    diameter = Column(String(100)) 
+    climate = Column(String(100)) 
+    gravity = Column(String(100)) 
+    terrain = Column(String(100))
+    surface_water = Column(String(100))
+    population = Column(String(100))
+    
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
